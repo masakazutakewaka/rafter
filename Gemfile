0afd2c3a71b4e34341c683eb92c60ca771320c9c
masakazutakewaka <takewakamma@gmail.com>
2019-04-26 00:16:06 +0900

    initial implementation

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
 
 # Specify your gem's dependencies in rafter.gemspec
+gem 'ridgepole'
 gemspec
--- lib/rafter.rb
+++ lib/rafter.rb
@@ -1,6 +1,14 @@
 require "rafter/version"
+require 'ridgepole'
 
 module Rafter
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.build_schema
+    conn_spec = YAML.load(File.read('config/database.yml').to_s)['test']
+    client = Ridgepole::Client.new(conn_spec)
+    dsl = File.read('db/Schemafile.rb')
+    delta = client.diff(dsl)
+    delta.migrate
+  end
 end
